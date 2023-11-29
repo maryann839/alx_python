50562d703c56b9e84d4e5e4cc96d384fb71e4b25
maryann839 <maryannokoye839@gmail.com>
2023-11-29 22:17:45 +0100

    commmmm

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -2,18 +2,13 @@
 import random
 number = random.randint(-10, 10)
 # YOUR CODE HERE
-# this is a positive number
-# number = 98
-# if number > 0:
-#   print("{} is positive".format(number))
-# elif number == 0:
-#    print("{} is zero".format(number))
-# elif number < 0:
-#   print("{} is negative".format(number))
-# number = 98
-# if number != 0:
-#   print("TypeError")
-# import sys
+if number > 0:
+  print("{} is positive".format(number))
+elif number == 0:
+   print("{} is zero".format(number))
+elif number < 0:
+  print("{} is negative".format(number))
+if number != 0:
+  print("TypeError")
 
-#Check if the number is positive, negative, or zero
-print(f"{number} is {'positive' if number > 0 else 'zero' if number == 0 else 'negative'}")
+
--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -2,6 +2,11 @@
 import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
-last_digit= int(repr(4567[-1]))
-if number > 5:
-   print('last digit of', 4567, 'is', last_digit, 'and is 0')
+last_digit= abs(number)% 10
+print('last digit of', number, 'is', last_digit , 'and is 0')
+if last_digit > 5:
+   print('last digit of', number, 'is', last_digit , 'and is greater than 6')
+elif last_digit == 0:
+   print('last digit of', number, 'is', last_digit , 'and is 0')
+elif last_digit < 6 and last_digit != 0:
+   print('last digit of', number, 'is', last_digit , 'and is less than 6 and not 0')
