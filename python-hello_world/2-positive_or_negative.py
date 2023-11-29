1c8cf4a4e6adb61df59c9cefee482fc270e0d346
maryann839 <maryannokoye839@gmail.com>
2023-11-29 18:25:17 +0100

    number

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -2,10 +2,18 @@
 import random
 number = random.randint(-10, 10)
 # YOUR CODE HERE
-number = 5
+number = 98
 if number > 0:
-  print("is positive")
-if number == 0:
-  print("is zero")
+  print("{} is positive".format(number), end="\n")
+import sys
+number = -98
 if number < 0:
-  print("is negative")
+   print("{} is negative".format(number), end="\n")
+   import sys
+number = 0
+if number == 0:
+  print("{} is zero".format(number))
+  import sys
+number = 98
+if number is 0:
+  print(is zero)
