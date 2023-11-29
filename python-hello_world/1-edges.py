aa4e8fcb2c506f0f0db2864c970e157d139ea44b
maryann839 <maryannokoye839@gmail.com>
2023-11-29 17:51:32 +0100

    bna

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-word = "Holberton"
-print("First 3 letters: {}".format(word[0:3]))
-print("Last 2 letters: {}".format(word[7:]))
-print("Middle word: {}".format(word[1:8]), end="\n")
-import sys
 word = "School"
 print("First 3 letters: {}".format(word[0:3]))
 print("Last 2 letters: {}".format(word[4:]))
