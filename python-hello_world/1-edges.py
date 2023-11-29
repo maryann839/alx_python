7e855a6952432c9ac415933122b6f4d3208053de
maryann839 <maryannokoye839@gmail.com>
2023-11-29 17:42:11 +0100

    commit

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,5 +1,19 @@
+# #!/usr/bin/python3
+# word = "Holberton"
+# print((word[0:3]))
+# print((word[7:]))
+# print(word[1:8])
+
 #!/usr/bin/python3
 word = "Holberton"
-print((word[0:3]))
-print((word[7:]))
-print(word[1:8])
+print("First 3 letters: {}".format(word[0:3]))
+print("Last 2 letters: {}".format(word[7:]))
+print("Middle word: {}".format(word[1:8]), end="\n")
+print(end="\n")
+import sys
+#!/usr/bin/python3
+word = "School"
+print("First 3 letters: {}".format(word[0:3]))
+print("Last 2 letters: {}".format(word[4:]))
+print("Middle word: {}".format(word[1:5]))
+import sys
