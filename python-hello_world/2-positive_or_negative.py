630936042aa5bb6728329423b12ac5d2eeee8217
maryann839 <maryannokoye839@gmail.com>
2023-11-29 18:09:03 +0100

    baaar

---
+++ python-hello_world/2-positive_or_negative.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+# YOUR CODE HERE
+number = 5
+if number > 0:
+  print("is positive")
+if number == 0:
+  print("is zero")
+if number < 0:
+  print("is negative")
