8d5d65bc51e67ac392932ab41beb09756cb16c38
maryann839 <maryannokoye839@gmail.com>
2023-11-29 17:55:32 +0100

    smart

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -4,9 +4,3 @@ print("First 3 letters: {}".format(word[0:3]))
 print("Last 2 letters: {}".format(word[7:]))
 print("Middle word: {}".format(word[1:8]), end="\n")
 import sys
-
-institution = "School"
-print("First 3 letters: {}".format(word[0:3]))
-print("Last 2 letters: {}".format(word[4:]))
-print("Middle word: {}".format(word[1:5]), end="\ns")
-import sys
