b460621b8e5c873e90eac4f30692d89bd85f1cca
maryann839 <maryannokoye839@gmail.com>
2023-11-29 20:52:47 +0100

    push

--- python-hello_world/0-print.py
+++ python-hello_world/0-print.py
@@ -1,3 +1,14 @@
 print("\"Programming is like building a multilingual puzzle", end="\n")
 import sys
 
+# #!/usr/bin/python3
+# word = "School"
+# print("First 3 letters: {}".format(word[0:3]))
+# print("Last 2 letters: {}".format(word[4:]))
+# print("Middle word: {}".format(word[1:5]))
+# import sys
+word = "Holberton"
+print("First 3 letters: {}".format(word[0:3]))
+print("Last 2 letters: {}".format(word[7:]))
+print("Middle word: {}".format(word[1:8]), end="\n")
+import sys
--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -2,6 +2,6 @@
 import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
-last_digit= int(repr(number[-1]))
+last_digit= int(repr(4567[-1]))
 if number > 5:
-   print('last digit of',number, 'is', last_digit, 'and is 0')
+   print('last digit of', 4567, 'is', last_digit, 'and is 0')
