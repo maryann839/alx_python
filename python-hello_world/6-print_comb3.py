f5abdd5eecfa74062d4e177f3121c151e978ab0a
maryann839 <maryannokoye839@gmail.com>
2023-11-29 21:34:17 +0100

    commit

---
+++ python-hello_world/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+
+number = random.randint(-10, 10)
+
+# Check if the number is positive, negative, or zero
+print(f"{number} is {'positive' if number > 0 else 'zero' if number == 0 else 'negative'}")
+
+# Check if the number is positive, negative, or zero
+print(f"{number} is {'positive' if number > 0 else 'zero' if number == 0 else 'negative'}")
