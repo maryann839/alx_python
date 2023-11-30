7ebdf0013781e0397e2120f1e8fb0d9a1cec804f
maryann839 <maryannokoye839@gmail.com>
2023-11-30 20:13:21 +0100

    push

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -2,13 +2,16 @@
 import random
 number = random.randint(-10, 10)
 # YOUR CODE HERE
+print(number, end=" ")
 if number > 0:
   print("{} is positive".format(number))
 elif number == 0:
    print("{} is zero".format(number))
-elif number < 0:
-  print("{} is negative".format(number))
-if number != 0:
+elif number != 0:
   print("TypeError")
+else:
+  print("{} is negative".format(number))
+# if number != 0:
+#   print("TypeError")
 
 
