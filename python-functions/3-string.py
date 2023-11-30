87c06726667ee86962cdb79c0af582a4475c9351
maryann839 <maryannokoye839@gmail.com>
2023-11-30 17:05:54 +0100

    push

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -1,6 +1,6 @@
 def reverse_string(string):
     reversed_str = ""
     for char in string:
-        reversed_str = char + reverse_str 
+        reversed_str = char + reversed_str 
     return reversed_str
 
