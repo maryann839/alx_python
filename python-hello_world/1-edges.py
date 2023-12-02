a7a99f92d94727cbeff82b8750082e196d6c141c
maryann839 <maryannokoye839@gmail.com>
2023-12-02 22:16:06 +0100

    pushhhhhh

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,4 +1,4 @@
-word = "Holberton"
+word = "Holberton"  "school"
 print("First 3 letters: {}".format(word[0:3]))
 print("Last 2 letters: {}".format(word[7:]))
 print("Middle word: {}".format(word[1:8]), end="\n") 
