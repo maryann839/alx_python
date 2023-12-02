982122a807bd18bb74a71f3e67fa406505c4787f
maryann839 <maryannokoye839@gmail.com>
2023-12-02 21:24:17 +0100

    pud

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-word = "Holberton"
+word = "School"
 print("First 3 letters: {}".format(word[0:3]))
-print("Last 2 letters: {}".format(word[7:]))
-print("Middle word: {}".format(word[1:8]), end="\n")
+print("Last 2 letters: {}".format(word[4:]))
+print("Middle word: {}".format(word[1:5]), end="\n")
 import sys
