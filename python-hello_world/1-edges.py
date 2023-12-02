a42f9c962af1b560415f96ffdf153bc4163d72b1
maryann839 <maryannokoye839@gmail.com>
2023-12-02 22:21:56 +0100

    comee

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -6,7 +6,3 @@ print("First 3 letters: {}".format(word[0:3]))
 print("Last 2 letters: {}".format(word[-2:]))
 print("Middle word: {}".format(word[1:-1]), end="\n")
 import sys
-# # word = "School"
-# print("First 3 letters: {}".format(word[0:3]))
-# print("Last 2 letters: {}".format(word[4:]))
-# print("Middle word: {}".format(word[1:5]), end="\n")
