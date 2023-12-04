c5f7a20550949721eb9d9051134aa0cf8d729ddc
maryann839 <maryannokoye839@gmail.com>
2023-12-04 20:38:30 +0100

    commit

---
+++ python-hello_world/0.py
@@ -0,0 +1,15 @@
+word = "SCHOOL"
+word_first_3, word_last_2, middle_word = word[:3], word[-2:], word[1:-1]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
+
+
+# word = "Holberton"
+# print("First 3 letters: {}".format(word[0:3]))
+# print("Last 2 letters: {}".format(word[7:]))
+# print("Middle word: {}".format(word[1:8]), end="\n") 
+# word = "School"
+# print("First 3 letters: {}".format(word[0:3]))
+# print("Last 2 letters: {}".format(word[4:]))
+# print("Middle word: {}".format(word[1:5]), end="\n")
--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -2,4 +2,4 @@
 for tens in range(10):
     for units in range(tens + 1, 10):
         print("{}{}".format(tens, units), end=", " if units < 89 else "\n")
-print()
+
