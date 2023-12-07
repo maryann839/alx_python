8db133ebaa774a9918e3f64646f1e6188aabacff
maryann839 <maryannokoye839@gmail.com>
2023-12-07 13:26:58 +0100

    push

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -4,6 +4,4 @@ def no_c(my_string):
       if char.lower() != "c":
          result += char
     return result
-print(no_c("Holberton School"))
-print(no_c("Chicago"))
-print(no_c("C is fun!")) 
+print(no_c("Holberton School"), end="\n")
