db4c020b1a1ef2c2aa9d4d7db594ca8a05d7acbf
maryann839 <maryannokoye839@gmail.com>
2023-12-07 20:00:55 +0100

    ssss

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -5,19 +5,11 @@ def safe_print_division(a, b):
       return None
    finally:
       print("Inside result: {}".format(result))
-      return result
+      if result is not None:
+         print("{} / {} = {}".format(a, b, result))
+      else:
+        print("{} / {} = None".format(a, b))
+     
    
-   #   print("{} / {} = {}".format(a, b, (a/ b)))
-# a = 10
-# b = 5
-# result = safe_print_division(a, b)
 # print("{} / {} = {}".format(a, b, (a/ b)))
 
-# try:
-#     x = 1 / 2
-# except ZeroDivisionError:
-#     print('Divided by zero.')
-# else:
-#     print('No exceptions raised.')
-# finally:
-#     print('This gets executed no matter what.')
