0706c35dc0d88465d78c8afaced8583eb2709d3e
maryann839 <maryannokoye839@gmail.com>
2023-12-07 11:48:23 +0100

    push

---
+++ python-import_modules/3-safe_print_division.py
@@ -0,0 +1,27 @@
+def safe_print_division(a, b):
+   try:
+      result = a / b
+   except ZeroDivisionError:
+      print('Division by Zero is invald')
+      raise
+   except Exception as e:
+      print('an error occured')
+      return None
+   else:
+      print("indide result: {}".format(result))
+      return result
+   finally:
+     pass
+a = 10
+b = 5
+result = safe_print_division(a, b)
+print("{} / {} = {}".format(a, b, (a/ b)))
+
+# try:
+#     x = 1 / 2
+# except ZeroDivisionError:
+#     print('Divided by zero.')
+# else:
+#     print('No exceptions raised.')
+# finally:
+#     print('This gets executed no matter what.')
