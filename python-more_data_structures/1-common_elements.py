3fe69af3d57925d16575e9e8e0935d5c238ef2af
maryann839 <maryannokoye839@gmail.com>
2023-12-07 16:19:05 +0100

    sett

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,5 +1,2 @@
 def common_elements(set_1, set_2):
-    # set_1 = {"bass", "tenor", "alto"}
-    # set_2 = {"classical", "opera", "afro"}
-    c_set = common_elements(set_1, set_2)
-    print(sorted(list(c_set)))
+    return set_1 & set_2
