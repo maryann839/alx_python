88fe77892dafba3842d1b2d69784fa4bfb442e75
maryann839 <maryannokoye839@gmail.com>
2023-12-07 14:07:39 +0100

    puddd

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,7 +1,6 @@
 def multiple_returns(sentence):
     if not isinstance(sentence, tuple) or not sentence:
        return None
-    length = len(sentence)
-    first = len(sentence[0])
+    length, first = multiple_returns(sentence)
     print("length: {} - First character:{}".format(length, first))
     
