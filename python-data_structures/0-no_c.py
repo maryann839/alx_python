f8ae2b3d84385a01143673a7945b666343b7ec3a
maryann839 <maryannokoye839@gmail.com>
2023-12-07 13:18:13 +0100

    meet

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,13 +1,9 @@
 def no_c(my_string):
-    print(no_c("Holberton School"))
-    print(no_c("Chicago"))
-    print(no_c("C is fun!"))
     result= ""
     for char in my_string:
       if char.lower() != "c":
          result += char
-          
-    print(no_c("Holberton School"))
-    print(no_c("Chicago"))
-    print(no_c("C is fun!"))
     return result
+print(no_c("Holberton School"))
+print(no_c("Chicago"))
+print(no_c("C is fun!")) 
