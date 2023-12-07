7e69d47a069078588add7e5ec9a228727ca6add3
maryann839 <maryannokoye839@gmail.com>
2023-12-07 19:05:55 +0100

    arg

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -2,11 +2,10 @@ import sys
 
 def display_arguments():
     num_arguments = len(sys.argv) - 1 
-    print("{} argument{}:".format(num_arguments, 's' if  num_arguments != 1 else ''))
-    print("." if num_arguments <= 0 else " ")
-    print()
+    plural = 's' if  num_arguments != 1 else ''
+    print("{} argument{}{}".format(num_arguments, plural, '' if num_arguments == 0 else 's'))
+   
   
-
     for i, arg in enumerate(sys.argv[1:], start=1):
         print("{}: {}".format(i, arg))
 if __name__ == "__main__":
