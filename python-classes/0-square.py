0b592fbe89a8a9151ee6a90a432ff4507bd1f5d8
maryann839 <maryannokoye839@gmail.com>
2023-12-13 15:24:38 +0100

    dash

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,15 +2,13 @@ class Square:
     def __init__(self, size):
         self.__size = size
 
-    def get_size(self):
-        return self.__size
+    # def get_size(self):
+    #     return self.__size
     
-    def set_size(self, size):
-        self.__size = size
+    # def set_size(self, size):
+    #     self.__size = size
 
-    # def area(self):
-    #     return self.__size**2
+square1 = Square()
 
-Square_instance = Square()
 
 
