228567f18367665f0ddec80b55418520652c7bb4
maryann839 <maryannokoye839@gmail.com>
2023-12-13 16:51:59 +0100

    bbbbb

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python3class Square:
-# creating  a class  
+"""
+this is the class Square
+"""
+ 
 class Square:
-    # specifying the attributes using __init__ 
-   def __init__(self, size=0):
-        self.__size = size
-   def get_size(self):
-       return self.__size
-   def set_size(self, size):
+    """
+    specifying the attributes using __init__ 
+    """ 
+    def __init__(self, size):
+        """
+        initializing a square instance
+
+        parameter - size
+        no type / value verification was used
+        """
         self.__size = size
 
-square1 = Square(0)
+    def get_size(self):
+        return self.__size
+    
+    def set_size(self, size):
+            self.__size = size
+
+square1 = Square()
 print("Size :", square1.get_size())
 
 
