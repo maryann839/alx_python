c6a56d28bb7465cbf28f6e287c7da3e6d3bfb6e6
maryann839 <maryannokoye839@gmail.com>
2023-12-13 16:37:55 +0100

    myyy

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -4,9 +4,13 @@ class Square:
     # specifying the attributes using __init__ 
    def __init__(self, size):
         self.__size = size
-
+   def get_size(self):
+       return self.__size
+   def set_size(self, size):
+        self.__size = size
 
 square1 = Square()
+print("Size :", square1.get_size())
 
 
 
