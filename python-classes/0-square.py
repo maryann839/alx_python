4bbf32842c919f7b5a3b0655bb43106546f092a0
maryann839 <maryannokoye839@gmail.com>
2023-12-13 14:31:18 +0100

    post

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,14 +2,14 @@ class Square:
     def __init__(self, size):
         self.__size = size
 
-    def get_size(self):
-        return self.__size
+    # def get_size(self):
+    #     return self.__size
     
-    def set_size(self, size):
-        self.__size = size
+    # def set_size(self, size):
+    #     self.__size = size
 
-    def area(self):
-        return self.__size**2
+    # def area(self):
+    #     return self.__size**2
 
 Square1 = Square()
 print()
