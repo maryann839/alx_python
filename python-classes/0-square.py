c94ba255564f7926fe3703c2c7d30f0f4d552a4f
maryann839 <maryannokoye839@gmail.com>
2023-12-13 13:47:58 +0100

    mnnn

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,6 @@
 class Square:
-    def __init__(self, size):
+    def __init__(self, size=3):
         self._size = size
-Square1 = Square()
-
+Square1 = Square(3)
+print(type(Square))
 print(Square1._size)
