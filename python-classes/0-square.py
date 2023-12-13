55fa33814618d07e6e5c1897c22b416c1b6f783e
maryann839 <maryannokoye839@gmail.com>
2023-12-13 14:14:50 +0100

    comit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,16 @@
 class Square:
-    def __init__(self, size=3):
+    def __init__(self, size):
         self.__size = size
+
+    def get_size(self):
+        return self.__size
+    
+    def set_size(self, size):
+        self.__size = size
+        
+    def area(self):
+        return self.__size**2
+
 Square1 = Square(3)
 
-print("_Square_size:", Square1.__size)
+print("_Square_size:", Square1.area)
