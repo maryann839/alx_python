927aa160e32e2633848acf137fdaa2e688008112
maryann839 <maryannokoye839@gmail.com>
2023-12-21 19:58:46 +0100

    BANN

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -2,4 +2,54 @@
 a class Rectangle that inherits from a base"""
 
 from models.base import Base
+
 class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ call the constructor of the base"""
+        super().__init__(id)
+
+        """
+        private attributes of the class"""
+        self.__width =width
+        self.__height = height
+        self.__x = x 
+        self.__y = y
+
+    def get_width(self):
+        return self.__width
+
+    def set_width(self, value):
+        self.__validate_interger(value, 'width')
+        self.__validate_positive(value, 'width')
+        self.__width = value
+    
+    def get_height(self):
+        return self.__height
+
+    def set_height(self, value):
+        self.__height = value
+
+    def get_x(self):
+        return self.__x
+
+    def set_x(self, value):
+        self.__x = value
+
+    def get_y(self):
+        return self.__x
+
+    def set_y(self, value):
+        self.__x = value
+    
+    def __str__(self):
+        return super().__str__()
+    
+    """
+    method of validation
+    """
+   
+    
+    
+
+
+
