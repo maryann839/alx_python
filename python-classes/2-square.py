1687526feeb48a0c2a9fa8f8675069667348b8a9
maryann839 <149114655+maryann839@users.noreply.github.com>
2024-02-05 14:29:54 +0100

    main

---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3class Square:
+
+""" this involves the creation of a class that defines 
+a square by a private  instance attributes, instantiation with  type/value
+a type error included and no module was imported """
+class Square:
+     """ specifying the attributes using __init__ """
+     def __init__(self, size=0):
+        if not isinstance(size, int):
+           raise TypeError("size must be an integer")
+        elif size < 0:
+           raise ValueError("size must be >= 0")
+        self.__size = size
+
+square1 = Square()
+
+def area(self):
+    return self.__size * self.__size
+
