2763df8949dc8cafcf3d9193bec04634a87c4f98
maryann839 <149114655+maryann839@users.noreply.github.com>
2024-02-05 15:32:20 +0100

    prrrirnntt

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -39,10 +39,12 @@ class Square:
          return self.__size ** 2
      
      def my_print(self):
-        for i in range(self.__size):
-           print("#"* self.__size)
         if self.__size == 0:
             print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
+
 
 """ this calculates the area of a square  """
 square1 = Square()
