d8a5d72472d39db59dcc897898a8db7ca551e301
maryann839 <149114655+maryann839@users.noreply.github.com>
2024-02-05 14:18:58 +0100

    equal

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -9,7 +9,7 @@ class Square:
         if not isinstance(size, int):
            raise TypeError("size must be an integer")
         elif size < 0:
-           raise ValueError("size must be >- 0")
+           raise ValueError("size must be >= 0")
         self.__size = size
 
 square1 = Square()
