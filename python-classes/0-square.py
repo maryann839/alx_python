e8c58868e5bb550c83a8839b24f6ede8e01cb320
maryann839 <149114655+maryann839@users.noreply.github.com>
2024-02-05 14:11:30 +0100

    mm

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -5,8 +5,8 @@ a square by a private  instance attributes, instantiation with no type/value
 and no module was imported """
   
 class Square:
-    # specifying the attributes using __init__ 
-    def __init__(self, size):
+   """ specifying the attributes using __init__ """
+   def __init__(self, size):
         # initializing a square instance
 
         # parameter - size
