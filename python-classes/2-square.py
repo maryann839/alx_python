56b0c1f200e1332e1838e84a6a98a9cc4177632a
maryann839 <149114655+maryann839@users.noreply.github.com>
2024-02-05 14:59:33 +0100

    moddd

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -11,16 +11,25 @@ class Square:
            raise TypeError("size must be an integer")
         elif size < 0:
            raise ValueError("size must be >= 0")
+        """ this involves the creation of a class that defines 
+        a square by a private  instance attributes, instantiation with  type/value
+        a type error included and no module was imported """
         self.__size = 0
         self.size = size 
 
      def area(self):
         """ this calculates the area of a square  """
+        """ this involves the creation of a class that defines 
+        a square by a private  instance attributes, instantiation with  type/value
+        a type error included and no module was imported """
         return self.__size ** 2
 
      @property
      def size(self):
         """ this assigns self.__size to size """
+        """ this involves the creation of a class that defines 
+        a square by a private  instance attributes, instantiation with  type/value
+        a type error included and no module was imported """
         return self.__size 
 
 square1 = Square()
