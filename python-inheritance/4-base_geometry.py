db87cc42cbec7c7a962a3749f9c4404c8849cc12
maryann839 <149114655+maryann839@users.noreply.github.com>
2024-02-05 16:43:46 +0100

    raise

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" improving the geometry class from previous file"""
+
+class BaseGeometry:
+    """ A basegeometry class"""
+    def area(self):
+        raise Exception("area() is not implemented")
+    
