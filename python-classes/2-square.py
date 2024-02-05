d9658eecf3cf7c7cfe591b2a8a560295d74c7e00
maryann839 <149114655+maryann839@users.noreply.github.com>
2024-02-05 15:07:05 +0100

    mmmmnnnn

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -9,33 +9,33 @@ a type error included and no msodule was imported """
 
 class Square:
      
-     """ specifying the attributes using __init__ """
+""" specifying the attributes using __init__ """
 
-     def __init__(self, size=0):
-        if not isinstance(size, int):
-           raise TypeError("size must be an integer")
-        elif size < 0:
-           raise ValueError("size must be >= 0")
-        
-        """ this involves the creation of a class that defines 
-        a square by a private  instance attributes, instantiation with  type/value
-        a type error included and no module was imported """
-        self.__size = 0
-        self.size = size 
+def __init__(self, size=0):
+if not isinstance(size, int):
+    raise TypeError("size must be an integer")
+elif size < 0:
+    raise ValueError("size must be >= 0")
 
-     def area(self):
-        """ this calculates the area of a square 
-         this involves the creation of a class that defines 
-        a square by a private  instance attributes, instantiation with  type/value
-        a type error included and no module was imported """
-        return self.__size ** 2
+""" this involves the creation of a class that defines 
+a square by a private  instance attributes, instantiation with  type/value
+a type error included and no module was imported """
+self.__size = 0
+self.size = size 
 
-     @property
-     def size(self):
-        """ this assigns self.__size to size """
-        """ this involves the creation of a class that defines 
-        a square by a private  instance attributes, instantiation with  type/value
-        a type error included and no module was imported """
-        return self.__size 
+def area(self):
+""" this calculates the area of a square 
+    this involves the creation of a class that defines 
+a square by a private  instance attributes, instantiation with  type/value
+a type error included and no module was imported """
+return self.__size ** 2
+
+@property
+def size(self):
+""" this assigns self.__size to size """
+""" this involves the creation of a class that defines 
+a square by a private  instance attributes, instantiation with  type/value
+a type error included and no module was imported """
+return self.__size 
 """ this calculates the area of a square  """
 square1 = Square()
