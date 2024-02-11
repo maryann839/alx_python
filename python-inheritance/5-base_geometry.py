b1c741efbcc65b00681bb019bf96753bfb9b185e
maryann839 <149114655+maryann839@users.noreply.github.com>
2024-02-11 19:36:47 +0100

    instance

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" improving the geometry class from previous file"""
+
+class BaseGeometry:
+    """ A basegeometry class"""
+
+    def area(self):
+        """ Not implemented """
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+         """ Vlaideate the value 
+         Args:
+            name (str): the name of the value 
+            value(int): the value to be validated
+         
+         Raises 
+            TypeError if not an interger
+            NameError if not a string"""
+         
+         if not isinstance(value, int):
+             raise TypeError
+         if value <= 0:
+             raise NameError
