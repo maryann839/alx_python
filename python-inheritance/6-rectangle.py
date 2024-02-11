85661387c84047d38bf5f554072dae71960a77cb
maryann839 <149114655+maryann839@users.noreply.github.com>
2024-02-11 19:59:13 +0100

    inter

---
+++ python-inheritance/6-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" improving the geometry class from previous file"""
+
+class BaseGeometry:
+    """ A basegeometry class"""
+
+    def area(self):
+        """ Not implemented """
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+         """ Vlaideate the value 
+         Args:
+            name (str): the name of the value 
+            value(int): the value to be validated
+         
+         Raises 
+            TypeError if not an interger
+            NameError if not a string"""
+         
+         if not isinstance(value, int):
+             raise TypeError("{} must be an integer".format(name))
+         if value <= 0:
+             raise ValueError("{} must be greater than 0".format(name))
+         
+    def __init__(self, width, height):
+        """ instantiation of wuidth and height"""
+        self.__width = self.integer_validator("width", width)
+        self.__height = self.integer_validator("height", height) 
