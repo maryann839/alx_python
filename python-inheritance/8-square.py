20d61fc4bb7dccdb3ffeb5f70e051f2d322b62e9
maryann839 <149114655+maryann839@users.noreply.github.com>
2024-02-11 20:53:32 +0100

    size

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -2,14 +2,14 @@
 """ A class that inherits 7-rectangle.py from previous file"""
 BaseGeometry = __import__('7-rectangle').BaseGeometry
 
-class Rectangle(BaseGeometry):
+class Square(BaseGeometry):
     """ A square class using basegeometry class
         A square class using basegeometry class"""
 
-    def __init__(self, width, height):
-        """ instantiation of wuidth and height"""
-        self.__width = self.integer_validator("width", width)
-        self.__height = self.integer_validator("height", height) 
+    def __init__(self, size):
+        """ instantiation of size"""
+        self.__size = self.integer_validator("size", size)
+     
 
     def area(self):
         """ print the area of the rectangle"""
