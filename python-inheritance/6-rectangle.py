274abcaff652028aa1af427123171ccd65f3fcdf
maryann839 <149114655+maryann839@users.noreply.github.com>
2024-02-11 20:07:09 +0100

    geo

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ A class that inherits 5-base_geometry.py from previous file"""
-BaseGeometry = __import__('5-base_geometry.py').BaseGeometry
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """ A basegeometry class"""
