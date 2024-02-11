b9746556b4e3ce344017488b9f79969109a6d39b
maryann839 <149114655+maryann839@users.noreply.github.com>
2024-02-11 19:13:42 +0100

    inhenrit

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -5,10 +5,7 @@ instance of a class that inherited (directly or indirectly) from the specified c
 
 def inherits_from(obj, a_class):
     """check if the object is an instance of the specified class"""
-    if type(obj) is a_class:
-        return False
+    if issubclass(type(obj, a_class) and type (obj) != a_class):
+        return True
     else:
-        for base_class in type(obj).__bases__:
-            if inherits_from(base_class, a_class):
-                return True
-        return False    
+         return False    
