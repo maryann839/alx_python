6995c040ed6fd43df5eba9b5f48693f992abfeda
maryann839 <149114655+maryann839@users.noreply.github.com>
2024-02-11 22:08:13 +0100

    recpy

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -192,8 +192,14 @@ class Rectangle(Base):
          validation of the model"""
           raise ValueError(f'{attribute_name} must be >= 0')
     
-   
-
+    def update(self, *args):
+        """ Update attributes"""
+        if args:
+            self.id = args[0] if len(args) > 0 else self.id
+            self.width = args[1] if len(args) > 1 else self.width
+            self.height = args[2] if len(args) > 2 else self.height
+            self.x = args[3] if len(args) > 3 else self.x
+            self.y = args[4] if len(args) > 4 else self.y
     
     
 
