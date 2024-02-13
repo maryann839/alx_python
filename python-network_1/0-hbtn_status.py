11d91362555c6d8535c9096f301af5506ef2f0bc
maryann839 <149114655+maryann839@users.noreply.github.com>
2024-02-13 13:49:54 +0100

    network

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
 """ request a url https://intranet.hbtn.io"""
 
-import urllib.request
+import requests
 
 
 if __name__== "__main__":
     url = "https://intranet.hbtn.io/status"
-    request = urllib.request.Request(url)
     
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print(f"\t- type: {type(body)}")
-        print(f"\t- content: {body}")
-        print(f"\t- utf8 content: {body.decode('utf-8')}")
+    response = request.get(url)
+    print("Body response:")
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
+    print(f"\t- utf8 content: {body.decode('utf-8')}")
