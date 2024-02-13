5b2cadaa1af3594940ab7e5cfdac6dbfed075267
maryann839 <149114655+maryann839@users.noreply.github.com>
2024-02-13 10:10:30 +0100

    sqyarepy

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,26 @@
+"""
+a class Square that inherits from a Rectangle"""
+
+from modes.rectangle import Rectangle
+
+class Square(Rectangle):
+    """ this class represents a square"""
+    def __init__(self, size, x=0, y=0, id=None):
+     """ initialize a new square"""
+     super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+       """ get the size of the square"""
+       return self.width
+    
+    @size.setter
+    def size(self, value):
+       self.width = value
+       self.height = value
+
+    def __str__(self):
+       """ Return the string """
+       return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}" 
+
+
