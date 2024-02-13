eec54681fcd53a9194770518750fb47078f38ed5
maryann839 <149114655+maryann839@users.noreply.github.com>
2024-02-13 14:59:22 +0100

    git

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -5,7 +5,7 @@ import sys
 if __name__== "__main__":
     username = sys.argv[1]
     personal_access_token = sys.argv[2]
-    url = 'https://api.hithub.com/user'
+    url = 'https://api.github.com/user'
     auth = (username, personal_access_token)
     response = requests.get(url, auth=auth)
     try:
