9f4bff190c1f054f82cb5a854b7f70b068982a94
maryann839 <149114655+maryann839@users.noreply.github.com>
2024-02-14 14:34:47 +0100

    base5

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -3,6 +3,14 @@
 
 class BaseGeometry:
     """ A basegeometry class"""
+    def __dir__(cls) -> None:
+        """list all attribute except the __init_subclass__"""
+        attributes = super().__dir__()
+        dir_to_return = []
+        for attribute in attributes:
+            if attribute != "__init_subclass__":
+                dir_to_return.append(attribute)
+        return dir_to_return
 
     def area(self):
         """ Not implemented """
