cb27ccaac57cab5f4c959392502b69555e0330f1
maryann839 <maryannokoye839@gmail.com>
2024-02-24 21:59:47 +0100

    recorrd

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,12 +10,28 @@ database = "hbtn_0e_0_usa"
 
 connection = MySQLdb.connect(host=host, port=port, user=username, passwd=password, database=database)
 cursor = connection.cursor()
+import MySQLdb
+
+
+host = 'localhost'
+port = 3306
+username = 'root'
+password = '@Nadiaann839'
+database = "hbtn_0e_0_usa"
+
+
+connection = MySQLdb.connect(host=host, port=port, user=username, passwd=password, database=database)
+cursor = connection.cursor()
 cursor.execute("SELECT * FROM states")
 
 records = cursor.fetchall()
 
 for x in records:
     print(x)
+
+
+
+
 
 
 
