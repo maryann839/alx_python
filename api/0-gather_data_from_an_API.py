ad50cea8fdf06d99ff7bd8c3503e91e83a0f74d3
maryann839 <maryannokoye839@gmail.com>
2024-02-28 11:20:43 +0100

    commit

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,10 +8,9 @@ def get_employee_info(employee_id):
     employee_url = "https://jsonplaceholder.typicode.com/users/{}"
     employee_response = requests.get(employee_url.format(employee_id))
      
-    """ if staus code is correct get employee details"""
+    #  if status code is correct get employee details
     if employee_response.status_code == 200:
         employee_details = employee_response.json()
-        employee_name = employee_details['name']
     else: 
         print(f"Failed to retrieve employee's details:{employee_response.status_code}")
         return None
@@ -26,19 +25,19 @@ def get_employee_info(employee_id):
         print(f"Failed to retrieve employee's todos:{employee_response.status_code}")
         return None
     
-    # employee_name = employee_details['name']
+    EMPLOYEE_NAME = employee_details['name']
     completed_tasks = sum(todo['completed'] for todo in todo_data)
-    number_of_done_tasks = completed_tasks
-    total_number_of_tasks = len(todo_data)
+    NUMBER_OF_DONE_TASKS = completed_tasks
+    TOTAL_NUMBER_OF_TASKS = len(todo_data)
     
-    print(f"Employee {employee_name} is done with tasks {number_of_done_tasks}/{total_number_of_tasks}:")
+    print(f"Employee {EMPLOYEE_NAME} is done with tasks {NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}:")
     for task in todo_data:
         print(f"\t {task['title']}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python script.py <employee_id>")
-        sys.exit(1)  # Exit with an error code
+        sys.exit(1)  
     employee_id = int(sys.argv[1])
     get_employee_info(employee_id)
 
