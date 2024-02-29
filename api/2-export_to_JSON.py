f84946a806cbda4697b9a2498f0a82721bc0eb01
maryann839 <maryannokoye839@gmail.com>
2024-02-29 09:06:31 +0100

    json

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3 
 
-import csv
-import requests 
-import sys 
+import json
+import requests
+import sys
 
-def get_employee_tasks(user_id):
+def get_export_json(user_id):
     """Get the endpoint"""
     employee_url = "https://jsonplaceholder.typicode.com/users/{}"
     employee_response = requests.get(employee_url.format(user_id))
@@ -12,51 +12,45 @@ def get_employee_tasks(user_id):
     #  if status code is correct get employee details
     if employee_response.status_code == 200:
         employee_details = employee_response.json()
-        user_id = employee_details['id']
         username = employee_details['username']
+
     else: 
         print(f"Failed to retrieve employee's details:{employee_response.status_code}")
         return None
-    
     
     todo_url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
     todo_response = requests.get(todo_url)
     
 
     if todo_response.status_code == 200:
-        task_data = todo_response.json()
+        todo_data = todo_response.json()
     else:
         print(f"Failed to retrieve employee's todos:{employee_response.status_code}")
         return None
-
-    csv_filename = f"{user_id}.csv"
-    with open(csv_filename, 'w', newline="") as csvfile:
-         fieldnames = ['user_id', 'username', 'task_completed_status', 'task_title']
-         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         
-         row = [{
-                 'user_id': user_id,
-                 'username': username,
-                 'task_completed_status': task['completed'],
-                 'task_title': task['title']
-                 } for task in task_data]
-         writer.writerows(row)
-                 
-             
-    print(f"The csv_file{csv_filename} was successfully created with data from {user_id}")
+    
+    employee_tasks = []
+    for task in todo_data:
+        employee_tasks.append( {
+            "task": task['title'],
+            "completed": task['completed'],
+            "username": username})
+        
+
+    json_filename = f"{user_id}.json"
+    with open(json_filename, "w") as json_file:
+        json.dump(employee_tasks, json_file, indent=2)
+
+    print(f"the json file {json_filename} has been created using {user_id}")
+       
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
          user_id = int(sys.argv[1])
     else:
       user_id = 1
-      get_employee_tasks(user_id)
-         
-
-        
-    
-
-
+      get_export_json(user_id)
 
 
 
