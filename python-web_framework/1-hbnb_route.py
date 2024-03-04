065344192f925b4ed0bb58a9f0f85aca0a6c52b6
maryann839 <maryannokoye839@gmail.com>
2024-03-04 16:09:29 +0100

    hbnh

---
+++ python-web_framework/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#! usr/bin/python3
+"""
+flask application
+"""
+
+#import flask
+from flask import Flask
+
+# make flask run file name
+app =  Flask(__name__)
+
+@app.route("/")
+def hello_hbnb():
+    return "Hello HBNB!"
+
+@app.route("/hbnb")
+def hbnb():
+    return "HBNB!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# debug=True, host="0.0.0.0", port="5000"
