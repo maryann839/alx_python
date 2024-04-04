3ef6744cced322e6faffb93877327123684d53cb
maryann839 <maryannokoye839@gmail.com>
2024-04-04 22:24:43 +0100

    orm

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,19 @@
 import MySQLdb
+from urllib.parse import quote_plus
 import sys
 
-print(sys.argv)
-host = 'localhost'
-port = 3306
+
+if len(sys.argv) != 4:
+    print("Usage: python 8-add_retrieve_users.py <db_username> <db_password> <db_name>")
+    sys.exit(1)
+
 username = sys.argv[1]
 password = sys.argv[2]
 database = sys.argv[3]
+host = 'localhost'
+port = 3306
 
-
+encoded_password = quote_plus(password)
 
 connection = MySQLdb.connect(host=host, port=port, user=username, passwd=password, database=database)
 cursor = connection.cursor()
