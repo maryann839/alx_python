4e547986115f17548fa0346b6351a5b97eb90abc
maryann839 <maryannokoye839@gmail.com>
2024-04-04 23:08:47 +0100

    ciiities

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -3,12 +3,13 @@ from urllib.parse import quote_plus
 import sys
 
 if len(sys.argv) != 5:
-    print("Usage: python 8-add_retrieve_users.py <db_username> <db_password> <db_name>")
+    print("Usage: python script_name.py <db_username> <db_password> <db_name> <state_name>")
     sys.exit(1)
 
 username = sys.argv[1]
 password = sys.argv[2]
 database = sys.argv[3]
+state_name = sys.argv[4]
 host = 'localhost'
 port = 3306
 
@@ -17,24 +18,21 @@ encoded_password = quote_plus(password)
 try:
     connection = MySQLdb.connect(host=host, port=port, user=username, passwd=password, database=database)
     cursor = connection.cursor()
-
-    # Execute the query to fetch cities
+    
+    # Execute the query to fetch cities of the given state
     query = """
-            SELECT name
+            SELECT cities.id, cities.name
             FROM cities
-            WHERE state_id IN (
-                SELECT id
-                FROM states
-                WHERE name = %s
-            )
-            ORDER BY id ASC
+            JOIN states ON cities.state_id = states.id
+            WHERE states.name = %s
+            ORDER BY cities.id ASC
             """
-    cursor.execute(query, ("Texas",))  # Replace "Texas" with the state_name variable
+    cursor.execute(query, (state_name,))
 
     records = cursor.fetchall()
-
+    
     # Store city names in a list
-    city_names = [record[0] for record in records]
+    city_names = [record[1] for record in records]
 
     # Print the city names joined by comma and space
     print(", ".join(city_names))
